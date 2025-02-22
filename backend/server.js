ef871decc8e5ea0952236c9ae5c1c411a76b1cd1
bhupendra gupta <134203340+bhupendra112@users.noreply.github.com>
2025-02-22 09:09:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,9 @@ const app = express();
 const port = process.env.PORT || 5000
 require("dotenv").config();
 require("./conn/conn")
-app.use(cors())
+app.use(cors(
+    origin:'https://task-manegment-1.onrender.com',
+))
     // Middleware to parse JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
